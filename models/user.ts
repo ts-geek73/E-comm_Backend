0be76e343f393c625936a3ffc994f332b9cd52ee
kushal-modi <kushal.modi@techstaunch.com>
2025-03-03 09:25:16 +0530

    first commit

---
+++ models/user.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+// Define the interface for the User document
+export interface IUser extends Document {
+  jwtToken: string;
+  clerkId: string;
+  userId: string;
+  sessionId: string;
+  username: string;
+  email: string;
+  provider: string;
+  roles?: string[];
+  permissions?: string[];
+  customClaim?: string;
+
+}
+
+const userSchema = new Schema<IUser>({
+  jwtToken : { type: String, required: true },
+  provider: { type: String, required: true },
+  clerkId: { type: String, required: true },
+  sessionId: { type: String, required: true },
+  userId: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  roles: [{ type: String }],
+  permissions: [{ type: String }],
+  customClaim: { type: String },
+});
+
+
+const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
