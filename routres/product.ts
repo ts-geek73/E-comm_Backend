3699839aa6f293116a488f72654d462035835655
kushal-modi <kushal.modi@techstaunch.com>
2025-04-29 16:39:08 +0530

    Limited Product with Filter and Sort Functionality

--- routres/product.ts
+++ routres/product.ts
@@ -9,6 +9,7 @@ router.get('/combos', productController.getBrandsandCategory as unknown as Reque
 router.get('/custome', productController.getLimitedProducts as unknown as RequestHandler );
 router.get('/search', productController.searchProduct as unknown as RequestHandler );
 router.put('/:id', productController.updateProduct as unknown as RequestHandler );
+router.get('/:id', productController.getProductById as unknown as RequestHandler );
 router.delete('/:id', productController.deleteProduct as unknown as RequestHandler );
 
 
